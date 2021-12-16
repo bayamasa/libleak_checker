5d309460cd088703ebd257aa9a5d769bd9494f55
kiri0314 <t.kirihara0314@gmail.com>
2021-12-16 13:20:56 +0900

    [fix]normにあわせてコードを修正

--- replace_leak.h
+++ replace_leak.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   replace_leek.h                                     :+:      :+:    :+:   */
+/*   replace_leak.h                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tkirihar <tkirihar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 11:57:20 by tkirihar          #+#    #+#             */
-/*   Updated: 2021/12/16 11:59:44 by tkirihar         ###   ########.fr       */
+/*   Updated: 2021/12/16 13:05:00 by tkirihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef REPLACE_LEEK_H
-# define REPLACE_LEEK_H
+#ifndef REPLACE_LEAK_H
+# define REPLACE_LEAK_H
 
 # define malloc(s) leak_detelc_malloc(s, __FILE__, __LINE__)
 # define free leak_detect_free
